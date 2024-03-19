72506e328c9d196017e83a52cb6eab09ae29ce7d
Kaustub Gawade <kaustubgawade@gmail.com>
2024-03-19 10:58:25 +0530

    First Commit

---
+++ src/main/java/com/blog/app/repositories/UserRepo.java
@@ -0,0 +1,13 @@
+package com.blog.app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blog.app.entities.User;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+	
+	
+	
+	
+}
